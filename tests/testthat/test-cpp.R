944d01eefc525adb84d2eb213a87d1fe975a9b32
Ralf Stubner <ralf.stubner@daqana.com>
2018-09-07 17:56:47 +0200

    Compile C++ API tests during the test run
    Using the 'catch' integration for C++ tests within 'testthat' is nice.
    But it is anoying that the tests code becomes part of the regular binary.

--- tests/testthat/test-cpp.R
+++ tests/testthat/test-cpp.R
@@ -1,4 +1,31 @@
 context("C++")
-test_that("Catch unit tests pass", {
-    expect_cpp_tests_pass("dqrng")
+
+Rcpp::sourceCpp("cpp/default.cpp")
+
+seed <- 1234567890
+
+test_that("consecutive calls yield different random numbers", {
+  expect_false(consecutive_calls(seed))
+})
+
+test_that("setting seed produces identical uniformly distributed numbers", {
+  expect_true(seed_uniform(seed))
+})
+
+test_that("setting seed produces identical normaly distributed numbers", {
+  expect_true(seed_normal(seed))
+})
+
+test_that("setting seed produces identical exponenetially distributed numbers", {
+  expect_true(seed_exponential(seed))
+})
+
+Rcpp::sourceCpp("cpp/xoshiro-jump.cpp")
+
+test_that("jump() for xoroshiro128+ works", {
+  expect_true(xoroshiro_jump())
+})
+
+test_that("jump() for xoshiro256+ works", {
+  expect_true(xoshiro_jump())
 })
