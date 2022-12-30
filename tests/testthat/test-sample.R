2ce96db6bad568e5b1306d1b3f5d0c56f335b10c
Ralf Stubner <ralf.stubner@gmail.com>
2022-12-30 14:26:27 +0100

    Add first test w/ weighted sampling

--- tests/testthat/test-sample.R
+++ tests/testthat/test-sample.R
@@ -115,12 +115,14 @@ test_that("dqsample_num w/o replacement works with medium rate", {
     expect_true(all(result >= 1) && all(result <= n))
 })
 
-test_that("sampling with weights gives warning", {
+test_that("dqsample_int w/o replacement and w/ weights works with medium rate", {
     dqset.seed(seed)
     n <- 1e5
     k <- 1e3
-    expect_warning(dqsample(n, k, replace = TRUE, prob = dqrunif(n)),
-                   "Using 'prob' is not supported yet. Using default 'sample.int'.")
+    result <- dqsample(n, k, replace = FALSE, prob = dqrunif(n))
+    expect_equal(length(result), k)
+    expect_equal(length(unique(result)), k)
+    expect_true(all(result >= 1) && all(result <= n))
 })
 
 test_that("error cases", {
