d68ea48f2a74d95505f79ccf712c95a82834ec07
Ralf Stubner <ralf.stubner@gmail.com>
2023-07-02 21:13:48 +0200

    Compare output from dqrrademacher with integer vector.

--- tests/testthat/test-rademacher.R
+++ tests/testthat/test-rademacher.R
@@ -3,6 +3,6 @@ context("rademacher")
 test_that("rademacher produces -1, 1", {
     out <- dqrrademacher(10)
     unique_out <- unique(out)
-    expect_identical(unique_out[order(unique_out)], c(-1, 1))
+    expect_identical(unique_out[order(unique_out)], c(-1L, 1L))
 })
 
