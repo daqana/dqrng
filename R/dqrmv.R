84ee090a25c3f39136cf82fac1ef09295fada12d
Ralf Stubner <ralf.stubner@gmail.com>
2023-08-27 12:41:19 +0000

    New methods `dqrmvnorm` and `dqrmvt` (#51)
    New method `dqrmvnorm`
    
    Originally dqrmvt making use of rmvt was also part of this PR. However, internally rmvt uses both rmvnorm and rchisq. The former is fine, since we were passing in our dqrnorm. However, the latter still uses R's RNG.
    
    In order to use this, we need to implement dqrchisq and mvtnorm::rmvt needs an option rchisq=rchisq.

---
+++ R/dqrmv.R
@@ -0,0 +1,26 @@
+#' @title Multivariate Distributions
+#'
+#' @param n  number of  observations
+#' @param ... forwarded to \code{\link[mvtnorm]{rmvnorm}} or \code{\link[mvtnorm]{rmvt}}
+#'
+#' @return numeric matrix of multivariate normal distributed variables
+#' @seealso \code{\link[mvtnorm]{rmvnorm}}
+#'
+#' @export
+#'
+#' @rdname dqrmv
+#' @examples
+#' sigma <- matrix(c(4,2,2,3), ncol=2)
+#' x <- dqrmvnorm(n=500, mean=c(1,2), sigma=sigma)
+#' colMeans(x)
+#' var(x)
+#' plot(x)
+dqrmvnorm <- function(n, ...) {
+  if (!requireNamespace("mvtnorm", quietly = TRUE)) {
+    stop(
+      "Package \"mvtnorm\" must be installed to use this function.",
+      call. = FALSE
+    )
+  }
+  mvtnorm::rmvnorm(n, ..., rnorm = dqrnorm)
+}
--- tests/testthat/test-rademacher.R
+++ tests/testthat/test-rademacher.R
@@ -6,3 +6,9 @@ test_that("rademacher produces -1, 1", {
     expect_identical(unique_out[order(unique_out)], c(-1L, 1L))
 })
 
+test_that("rademacher produces many -1, 1", {
+    out <- dqrrademacher(1e6)
+    unique_out <- unique(out)
+    expect_identical(unique_out[order(unique_out)], c(-1L, 1L))
+})
+
