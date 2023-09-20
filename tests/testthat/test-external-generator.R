7a0034161709383f9dd31c1b210c587faa237e2f
hsloot <16246049+hsloot@users.noreply.github.com>
2023-09-20 15:49:48 +0200

    test: add draft for external RNG

---
+++ tests/testthat/test-external-generator.R
@@ -0,0 +1,37 @@
+context("external-generators")
+
+Rcpp::sourceCpp("cpp/external-generator.cpp")
+
+test_that("global RNG is accessible and works as expected", {
+  n <- 1e2L
+  rate <- 0.4
+  use_seed <- 1623
+
+  dqset.seed(use_seed)
+  expected <- dqrexp(n, rate)
+  dqset.seed(use_seed)
+  actual <- test_dqrexp(n, rate)
+  expect_equal(actual, expected)
+
+  dqset.seed(use_seed)
+  expected2 <- expected
+  actual2 <- sapply(1:n, function(x) test_dqrexp(1, rate))
+  expect_equal(actual2, expected2)
+
+  cl <- parallel::makeCluster(2)
+  expected3 <- parallel::clusterApply(cl, 1:8, function(stream, seed, N, rate) {
+    dqrng::dqRNGkind("Threefry")
+    dqrng::dqset.seed(seed, stream)
+    dqrng::dqrexp(N, rate)
+  }, use_seed, 1e6L, rate)
+  parallel::stopCluster(cl)
+  cl <- parallel::makeCluster(2)
+  actual3 <- parallel::clusterApply(cl, 1:8, function(stream, seed, N, rate) {
+    Rcpp::sourceCpp("cpp/external-generator.cpp") ## must be recompiled
+    dqrng::dqRNGkind("Threefry")
+    dqrng::dqset.seed(seed, stream)
+    test_dqrexp(N, rate)
+  }, use_seed, 1e6L, rate)
+  parallel::stopCluster(cl)
+  expect_equal(actual3, expected3)
+})
