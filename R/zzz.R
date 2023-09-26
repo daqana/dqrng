2536fffbe57ee99b29a08c4f7db8f66e31def5a7
Ralf Stubner <ralf.stubner@gmail.com>
2023-09-26 22:56:03 +0200

    Register RNG as user-defined if option is set
    The RNG get's initialized by R via user_unif_init() making it unnecessary to call dqset_seed(NULL).

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,11 @@
 .onLoad <- function(libname, pkgname) {
-  if (!exists(".Random.seed", mode="numeric", envir=globalenv()))
-    set.seed(NULL)
-  original_seed <- get(".Random.seed", mode="numeric", envir=globalenv())
-  on.exit(assign(".Random.seed", original_seed, envir=globalenv()))
-  dqset_seed(seed = NULL, stream = NULL)
+  if (getOption("dqrng.register_methods", FALSE))
+    RNGkind("user", "user")
+  else {
+    if (!exists(".Random.seed", mode="numeric", envir=globalenv()))
+      set.seed(NULL)
+    original_seed <- get(".Random.seed", mode="numeric", envir=globalenv())
+    on.exit(assign(".Random.seed", original_seed, envir=globalenv()))
+    dqset_seed(seed = NULL, stream = NULL)
+  }
 }
