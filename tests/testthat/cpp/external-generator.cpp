82715b0bf68784d749ab3debf465b61b20b3711b
Ralf Stubner <ralf.stubner@gmail.com>
2024-04-15 10:55:33 +0200

    Add test for clone(stream) method

--- tests/testthat/cpp/external-generator.cpp
+++ tests/testthat/cpp/external-generator.cpp
@@ -11,3 +11,17 @@ Rcpp::NumericVector dqrexp_extrng(const std::size_t n, const double rate = 1.0) 
   engine.generate<dqrng::exponential_distribution>(out, rate);
   return out;
 }
+
+// [[Rcpp::export]]
+bool cloned_calls(int stream) {
+  dqrng::random_64bit_accessor engine{};
+  auto cloned_engine = engine.clone(stream);
+
+  Rcpp::NumericVector u1(10);
+  Rcpp::NumericVector u2(10);
+
+  engine.generate<dqrng::uniform_distribution>(u1);
+  cloned_engine->generate<dqrng::uniform_distribution>(u2);
+
+  return Rcpp::is_true(Rcpp::all(u1 == u2));
+}
