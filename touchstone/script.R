482687a115f8b238c1ea93ef1633203ae41d406c
Ralf Stubner <ralf.stubner@gmail.com>
2024-04-23 18:59:56 +0200

    Update performance testing

--- touchstone/script.R
+++ touchstone/script.R
@@ -11,21 +11,21 @@ touchstone::branch_install()
 # benchmark a function call from your package (two calls per branch)
 touchstone::benchmark_run(
   # expr_before_benchmark = source("dir/data.R"), #<-- TODO OTPIONAL setup before benchmark
-  runif_test = dqrng::dqrunif(1e6), #<- TODO put the call you want to benchmark here
+  runif_test = dqrng::dqrunif(1e7), #<- TODO put the call you want to benchmark here
   n = 10
 )
 
 # benchmark a function call from your package (two calls per branch)
 touchstone::benchmark_run(
   # expr_before_benchmark = source("dir/data.R"), #<-- TODO OTPIONAL setup before benchmark
-  rnorm_test = dqrng::dqrnorm(1e6), #<- TODO put the call you want to benchmark here
+  rnorm_test = dqrng::dqrnorm(1e7), #<- TODO put the call you want to benchmark here
   n = 10
 )
 
 # benchmark a function call from your package (two calls per branch)
 touchstone::benchmark_run(
   # expr_before_benchmark = source("dir/data.R"), #<-- TODO OTPIONAL setup before benchmark
-  rexp_test = dqrng::dqrexp(1e6), #<- TODO put the call you want to benchmark here
+  rexp_test = dqrng::dqrexp(1e7), #<- TODO put the call you want to benchmark here
   n = 10
 )
 
